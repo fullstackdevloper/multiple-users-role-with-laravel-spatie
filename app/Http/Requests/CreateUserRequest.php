ad3ec3390e0694e9729b7d60e1c4f00e4f556fff
Shivi991-dev <141829444+Shivi991-dev@users.noreply.github.com>
2024-03-05 17:44:17 +0530

    changes

--- app/Http/Requests/CreateUserRequest.php
+++ app/Http/Requests/CreateUserRequest.php
@@ -25,7 +25,7 @@ class CreateUserRequest extends FormRequest
     {
         return [
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', Rule::unique(User::class)],
             'password' => ['required','string']
         ];
     }
