152e0c5bb0ff151a60258dbff7466d493323e9eb
fullstackdeveloper <ourdailywork@gmail.com>
2024-03-11 10:31:28 +0530

    feature test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,52 @@
 
 namespace Tests;
 
+use App\Models\User;
+use Hamcrest\Arrays\IsArray;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication, RefreshDatabase;
+
+    public function userWithPermissions($roleToAssign, $permissionsToAssign)
+    {
+        $user        = User::factory()->create();
+        $role        = $this->createRole($roleToAssign);
+        $permission  = $this->createPermission($permissionsToAssign);
+        if(is_array($permission)) {
+            echo "<pre>"; print_R($permission);
+        }
+        $role->syncPermissions($permission);
+        $user->syncRoles($role);
+        return $user;
+    }
+
+    public function userWithoutPermissions($roleToAssign)
+    {
+        $user       = User::factory()->create();
+        $role        = $this->createRole($roleToAssign);
+        $user->syncRoles($role);
+        return $user;
+    }
+
+    protected function createRole($role)
+    {
+        return Role::create(['name' => $role]);
+    }
+
+    protected function createPermission($permissions)
+    {
+        if (is_array($permissions)) {
+            foreach ($permissions as $permission) {
+                Permission::create(['name' => $permission]);
+            }
+            return Permission::pluck('name')->toArray();
+        } else {
+            return Permission::create(['name' => $permissions]);
+        }
+    }
 }
